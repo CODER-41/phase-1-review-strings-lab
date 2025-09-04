e200708cb76854860a486e03b6bdad7ce4bcda16
RONNY <mboyaronny41@gmail.com>
2025-09-04 08:45:56 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+//Write Your Code Here//
+const currentUser = 'Code Sorcerer';
+const welcomeMessage = 'Welcome to Flatbook, + ${Code Sorcerer}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, + {C.slice(0, 1)}!`;
